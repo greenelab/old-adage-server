5b1b16f3ed2c52aec82f818a2cfa2c5219627acf
Matt Huyck <mhuyck@fgtech.com>
2016-01-29 16:46:51 -0500

    removing code that is no longer needed
    I changed the way the search interface works. Also: a method for retrieving
    related items via the Experiment and Sample APIs became unnecessary after I
    got the built-in relations to work properly.

--- adage/analyze/api.py
+++ adage/analyze/api.py
@@ -82,17 +82,6 @@ class SearchResource(Resource):
             object_list.append(new_obj)
         
         return object_list
-    
-    # def dehydrate_related_items(self, bundle):
-        # related_item_list = []
-        # print(bundle.obj.pk)
-        # if bundle.obj.item_type == 'experiment':
-        #     e = Experiment.objects.get(pk=bundle.obj.pk)
-        #     return e.sample_set
-        # elif bundle.obj.item_type == 'sample':
-        #     s = Sample.objects.get(pk=bundle.obj.pk)
-        #     return s.experiments
-        # return bundle.obj.related_items
 
 
 class ExperimentResource(ModelResource):
@@ -102,57 +91,6 @@ class ExperimentResource(ModelResource):
     class Meta:
         queryset = Experiment.objects.all()
         allowed_methods = ['get']
-    
-    def prepend_urls(self):
-        return [
-            url(r'^(?P<resource_name>%s)/search%s$' % \
-                    (self._meta.resource_name, trailing_slash()),
-                self.wrap_view('get_search'), name='api_get_search'),
-        ]
-    
-    def get_samples(self, request, pk=None, **kwargs):
-        if pk:
-            sample_obj = Experiment.objects.get(pk=pk)
-        objects = []
-        # TODO stopped here.
-        er = ExperimentResource()
-        for e in sample_obj.get_experiments():
-            bundle = er.build_bundle(obj=e, request=request)
-            bundle = er.full_dehydrate(bundle)
-            objects.append(bundle)
-        
-        return self.create_response(request, objects)
-    
-    def get_search(self, request, **kwargs):
-        """
-        provide a search API via haystack's SearchQuerySet API
-        Return: results to display with query terms highlighted
-        """
-        self.method_check(request, allowed=['get'])
-        self.throttle_check(request)
-        
-        # unpack the query string from the request headers
-        query_str = request.GET.get('q', '')
-        
-        # restrict our search to the Experiment models
-        sqs = SearchQuerySet().models(Experiment)
-        
-        # run the query and specify we want highlighted results
-        sqs = sqs.filter(content=AutoQuery(query_str)).load_all().highlight()
-        
-        objects = []
-        for result in sqs:
-            # This is they way whoosh returns highlighted results
-            # snippet = { 'snippet': result.highlighted['text'][0] }
-            # This is the way elasticsearch returns highlighted results
-            snippet = {'snippet': ' ...'.join(result.highlighted)}
-            bundle = self.build_bundle(
-                    obj=result.object, data=snippet, request=request)
-            bundle = self.full_dehydrate(bundle)
-            objects.append(bundle)
-        
-        self.log_throttled_access(request)
-        return self.create_response(request, objects)
 
 
 class SampleResource(ModelResource):
