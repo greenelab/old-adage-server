304107e1a4da288439325f470b1345211b5966e3
Matt Huyck <mhuyck@fgtech.com>
2016-04-27 11:48:52 -0400

    Backing out rudimentary UI changes.
    These are about to be reworked so it doesn't make sense to review as part of
    the next pull request. When the Angular and Bootstrap libraries are updated
    these will be worked in again (in a manner consistent with updated JS APIs).

--- interface/src/app/analyze/analyze.js
+++ interface/src/app/analyze/analyze.js
@@ -35,34 +35,8 @@ angular.module( 'adage.analyze', [
     { 'query': { method: 'GET', isArray: false } }
   );
 }])
-.factory( 'Sample', ['$resource', '$http', function($resource, $http) {
-  console.log('About to try a get for sample @id');
-  return $resource(
-    '/api/v0/sample/:id/',
-    // TODO need to add logic for handling pagination of results.
-    // then, can change "limit" below to something sensible
-    { id: '@id', limit: 0 },
-    // Angular expects a query service to give only a list but our Tastypie
-    // interface wraps the response list with pagination so we tell Angular to
-    // expect an object instead via isArray: false
-    { 'get': { method: 'GET', isArray: false } }
-  );
-}])
-.factory( 'Experiment', ['$resource', '$http', function($resource, $http) {
-  console.log('About to try a get for experiment @id');
-  return $resource(
-    '/api/v0/experiment/:accession/',
-    // TODO need to add logic for handling pagination of results.
-    // then, can change "limit" below to something sensible
-    { accession: '@accession', limit: 0 },
-    // Angular expects a query service to give only a list but our Tastypie
-    // interface wraps the response list with pagination so we tell Angular to
-    // expect an object instead via isArray: false
-    { 'get': { method: 'GET', isArray: false } }
-  );
-}])
 
-.controller( 'AnalyzeCtrl', function AnalyzeCtrl( $scope, $modal, $log, Search ) {
+.controller( 'AnalyzeCtrl', function AnalyzeCtrl( $scope, Search ) {
   $scope.query = {
     text: "",
     results: [],
@@ -80,12 +54,7 @@ angular.module( 'adage.analyze', [
     Search.query({ q: $scope.query.text },
       function(response_object, responseHeaders) {
         object_list = response_object.objects;
-        if (object_list.length == 1) {
-          noun = " match.";
-        } else {
-          noun = " matches.";
-        }
-        $scope.query.status = "Found " + object_list.length + noun;
+        $scope.query.status = "Found " + object_list.length + " matches.";
         $scope.query.results = object_list;
       },
       function(response_object, responseHeaders) {
@@ -94,97 +63,6 @@ angular.module( 'adage.analyze', [
       }
     );
   };
-  
-  $scope.detail = {
-    search_item: {}
-  };
-  $scope.show_detail = function( search_item ) {
-    $scope.detail.search_item = search_item;
-    var modalInstance = $modal.open({
-      animation: false,
-      templateUrl: 'analyze/detailModal.tpl.html',
-      controller: 'DetailModalCtrl',
-      resolve: {
-        detail: function() {
-          return $scope.detail;
-        }
-      }
-    });
-    modalInstance.result.then(function (selectedItem) {
-      $log.info('Modal dismissed with selectedItem at: ' + new Date());
-    }, function () {
-      $log.info('Modal dismissed at: ' + new Date());
-    });
-  };
-  
-  $scope.analysis = {
-    sample_list: []
-  };
-  $scope.add_item = function( search_item ) {
-    $log.info('add_item: ' + search_item.item_type);
-    if (search_item.item_type == 'sample') {
-      $scope.analysis.sample_list.push(+search_item.pk);
-    } else if (search_item.item_type == 'experiment') {
-      $scope.analysis.sample_list = 
-        $scope.analysis.sample_list.concat(search_item.related_items);
-    }
-  };
-  $scope.show_analysis = function( ) {
-    var modalInstance = $modal.open({
-      animation: false,
-      templateUrl: 'analyze/analysisModal.tpl.html',
-      controller: 'AnalysisModalCtrl',
-      resolve: {
-        analysis: function() {
-          return $scope.analysis;
-        }
-      }
-    });
-    modalInstance.result.then(function (selectedItem) {
-      $log.info('Modal dismissed with selectedItem at: ' + new Date());
-    }, function () {
-      $log.info('Modal dismissed at: ' + new Date());
-    });
-  };
 })
 
-.controller('DetailModalCtrl', function($scope, $modalInstance, $log, detail, Sample, Experiment) {
-  $scope.detail = detail;
-  $scope.close = function() {
-    $modalInstance.dismiss('close');
-  };
-  if (detail.search_item.item_type == 'sample') {
-    Sample.get({ id: detail.search_item.pk },
-      function(response_object, responseHeaders) {
-        if (response_object) {
-          $scope.detail.status = "Found a match.";
-          $scope.detail.results = JSON.stringify(response_object, null, 4);
-        }
-      },
-      function(response_object, responseHeaders) {
-        console.log('Query errored with: ' + response_object);
-        $scope.detail.status = "Query failed.";
-      }
-    );
-  } else if (detail.search_item.item_type == 'experiment') {
-    Experiment.get({ accession: detail.search_item.pk },
-      function(response_object, responseHeaders) {
-        if (response_object) {
-          $scope.detail.status = "Found a match.";
-          $scope.detail.results = JSON.stringify(response_object, null, 4);
-        }
-      },
-      function(response_object, responseHeaders) {
-        console.log('Query errored with: ' + response_object);
-        $scope.detail.status = "Query failed.";
-      }
-    );
-  }
-})
-.controller('AnalysisModalCtrl', function($scope, $modalInstance, analysis) {
-  $scope.analysis = analysis;
-  $scope.close = function() {
-    $modalInstance.dismiss('close');
-  };
-})
 ;
