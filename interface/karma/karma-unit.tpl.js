efeb19ba83944c563a37c36660840fd9104d7418
Dongbo Hu <dongbo.hu@gmail.com>
2016-08-24 11:41:29 -0400

    Cut lines that are longer than 80 characters

--- interface/karma/karma-unit.tpl.js
+++ interface/karma/karma-unit.tpl.js
@@ -6,13 +6,14 @@ module.exports = function ( karma ) {
     basePath: '../',
 
     /**
-     * This is the list of file patterns to load into the browser during testing.
+     * This is the list of file patterns to load into the browser during
+     * testing.
      */
     files: [
       <% scripts.forEach( function ( file ) { %>'<%= file %>',
       <% }); %>
       'src/**/*.js',
-      //'src/**/*.coffee',  // Commented out to disable a warning message.
+      //'src/**/*.coffee', // Commented out to disable a warning in karma:unit.
     ],
     exclude: [
       'src/assets/**/*.js',
@@ -20,7 +21,8 @@ module.exports = function ( karma ) {
       'src/**/*.e2e.js',
     ],
     frameworks: [ 'jasmine' ],
-    plugins: [ 'karma-jasmine', 'karma-phantomjs-launcher', 'karma-coffee-preprocessor' ],
+    plugins: [ 'karma-jasmine', 'karma-phantomjs-launcher',
+               'karma-coffee-preprocessor' ],
     preprocessors: {
       '**/*.coffee': 'coffee',
     },
@@ -31,8 +33,8 @@ module.exports = function ( karma ) {
     reporters: 'dots',
 
     /**
-     * On which port should the browser connect, on which port is the test runner
-     * operating, and what is the URL path for the browser to use.
+     * On which port should the browser connect, on which port is the test
+     * runner operating, and what is the URL path for the browser to use.
      */
     port: 9018,
     runnerPort: 9100,
@@ -44,17 +46,19 @@ module.exports = function ( karma ) {
     autoWatch: false,
 
     /**
-     * The list of browsers to launch to test on. This includes only "Firefox" by
-     * default, but other browser names include:
+     * The list of browsers to launch to test on. This includes only "Firefox"
+     * by default, but other browser names include:
      * Chrome, ChromeCanary, Firefox, Opera, Safari, PhantomJS
      *
-     * Note that you can also use the executable name of the browser, like "chromium"
-     * or "firefox", but that these vary based on your operating system.
+     * Note that you can also use the executable name of the browser, like
+     * "chromium" or "firefox", but that these vary based on your operating
+     * system.
      *
      * You may also leave this blank and manually navigate your browser to
-     * http://localhost:9018/ when you're running tests. The window/tab can be left
-     * open and the tests will automatically occur there during the build. This has
-     * the aesthetic advantage of not launching a browser every time you save.
+     * http://localhost:9018/ when you're running tests. The window/tab can be
+     * left open and the tests will automatically occur there during the build.
+     * This has the aesthetic advantage of not launching a browser every time
+     * you save.
      */
     browsers: [ 'PhantomJS' ]
   });
