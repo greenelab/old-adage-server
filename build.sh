78aecca468ad40874517432445fb1d90f473bedc
Casey Greene <cgreene@users.noreply.github.com>
2016-09-22 09:32:24 -0400

    add explanation for weird bashness

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+# Avoid silent and/or consumed failures within a bash script.
+# if interested: http://redsymbol.net/articles/unofficial-bash-strict-mode/
 set -euo pipefail
 IFS=$'\n\t'
 
