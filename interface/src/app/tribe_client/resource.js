0227cc969fd249ab588e3d6bfa4a3750f12543da
Rene Zelaya <Rene.Zelaya.F@gmail.com>
2016-10-03 10:25:17 -0400

    Adding TribeSettings resource to tribe_client/resource.js to fetch things like location of Tribe instance and client_id, which are returned by a tribe_client url endpoint.

--- interface/src/app/tribe_client/resource.js
+++ interface/src/app/tribe_client/resource.js
@@ -5,4 +5,8 @@ angular.module("adage.tribe_client.resource", ['ngResource'])
     .factory('User', ['$resource', function($resource) {
         return $resource('/tribe_client/return_user', {}, {});
     }])
+
+    .factory('TribeSettings', ['$resource', function($resource) {
+        return $resource('/tribe_client/get_settings', {}, {});
+    }])
 ;
