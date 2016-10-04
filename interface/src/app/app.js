4bbb272f74c97df21f96409b1db7331ef17abc3d
Rene Zelaya <Rene.Zelaya.F@gmail.com>
2016-10-04 12:01:54 -0400

    Converting AppCtrl in main app/app.js file to use inline array annotation, and reducing the line length to comply with the 80-character limit.

--- interface/src/app/app.js
+++ interface/src/app/app.js
@@ -23,18 +23,20 @@ angular.module( 'adage', [
 .run( function run () {
 })
 
-.controller( 'AppCtrl', function AppCtrl ( $scope, UserFactory ) {
+.controller( 'AppCtrl', ['$scope', 'UserFactory',
+  function AppCtrl ( $scope, UserFactory ) {
 
-  $scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams){
-    if ( angular.isDefined( toState.data.pageTitle ) ) {
-      $scope.pageTitle = toState.data.pageTitle + ' | adage' ;
-    }
-  });
+    $scope.$on('$stateChangeSuccess',
+      function(event, toState, toParams, fromState, fromParams){
+        if ( angular.isDefined( toState.data.pageTitle ) ) {
+          $scope.pageTitle = toState.data.pageTitle + ' | adage' ;
+        }
+      });
 
-  UserFactory.getPromise().$promise.then( function() {
-      $scope.userObj = UserFactory.getUser();
-  });
+    UserFactory.getPromise().$promise.then( function() {
+        $scope.userObj = UserFactory.getUser();
+    });
 
-})
+}])
 
 ;
