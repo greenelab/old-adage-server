350f213245b414284bd27f86d10121a51f65ba1f
Rene Zelaya <Rene.Zelaya.F@gmail.com>
2016-10-14 13:48:48 -0400

    Reformatting tribe_client/directives.js so that the linter is happier, and also adding a TODO comment for the controller in the genesetSearchForm, which is still empty.

--- interface/src/app/tribe_client/directives.js
+++ interface/src/app/tribe_client/directives.js
@@ -4,82 +4,83 @@ angular.module('adage.tribe_client.directives', [
   'adage.tribe_client.utils'
 ])
 
-.directive('profileButton', ['UserFactory', 'CommonFuncts', function(UserFactory, CommonFuncts) {
-  return {
-    controller: ['$scope', 'UserFactory', function($scope, UserFactory) {
-
-      UserFactory.getPromise().$promise.then(function() {
-        $scope.userObj = UserFactory.getUser();
-      });
-
-    }],
-    link: CommonFuncts.updateUser,
-    replace: true,
-    restrict: "E",
-    templateUrl: 'tribe_client/tribe-profile-button.tpl.html'
-  };
-}])
-
-.directive('loginButton', ['UserFactory', 'CommonFuncts', function(UserFactory, CommonFuncts) {
-  return {
-    controller: ['$scope', 'UserFactory', '$uibModal', '$rootScope', '$window',
-      function($scope, UserFactory, $uibModal, $rootScope, $window) {
-
+.directive('profileButton', ['UserFactory', 'CommonFuncts',
+  function(UserFactory, CommonFuncts) {
+    return {
+      controller: ['$scope', 'UserFactory', function($scope, UserFactory) {
         UserFactory.getPromise().$promise.then(function() {
           $scope.userObj = UserFactory.getUser();
         });
+      }],
+      link: CommonFuncts.updateUser,
+      replace: true,
+      restrict: "E",
+      templateUrl: 'tribe_client/tribe-profile-button.tpl.html'
+    };
+  }
+])
 
-        $scope.openLoginModal = function() {
+.directive('loginButton', ['UserFactory', 'CommonFuncts',
+  function(UserFactory, CommonFuncts) {
+    return {
+      controller: ['$scope', 'UserFactory', '$uibModal', '$rootScope', '$window',
+        function($scope, UserFactory, $uibModal, $rootScope, $window) {
+          UserFactory.getPromise().$promise.then(function() {
+            $scope.userObj = UserFactory.getUser();
+          });
 
-          var modalInstance = $uibModal.open({
-            templateUrl: 'tribe_client/tribe-login-modal.tpl.html',
-            controller: [
-              '$scope', '$uibModalInstance', 'TribeSettings',
-              function($scope, $uibModalInstance, TribeSettings) {
+          $scope.openLoginModal = function() {
+            var modalInstance = $uibModal.open({
+              templateUrl: 'tribe_client/tribe-login-modal.tpl.html',
+              controller: [
+                '$scope', '$uibModalInstance', 'TribeSettings',
+                function($scope, $uibModalInstance, TribeSettings) {
+                  TribeSettings.get({}, function(data) {
+                    $scope.tribe_url = data['tribe_url'];
+                    $scope.access_code_url = data['access_code_url'];
+                    $scope.tribe_scope = data['scope'];
+                    $scope.client_id = data['client_id'];
+                  });
 
-                TribeSettings.get({}, function(data) {
-                  $scope.tribe_url = data['tribe_url'];
-                  $scope.access_code_url = data['access_code_url'];
-                  $scope.tribe_scope = data['scope'];
-                  $scope.client_id = data['client_id'];
-                });
+                  $scope.refresh = function() {
+                    $rootScope.$broadcast('user.update');
+                    $uibModalInstance.close();
+                    $window.location.reload();
+                  };
 
-                $scope.refresh = function() {
-                  $rootScope.$broadcast('user.update');
-                  $uibModalInstance.close();
-                  $window.location.reload();
-                };
+                  $scope.cancel = function() {
+                    $uibModalInstance.dismiss('cancel');
+                  };
+                }
+              ]
+            });
 
-                $scope.cancel = function() {
-                  $uibModalInstance.dismiss('cancel');
-                };
+            modalInstance.result.then(function(success) {
+              if (success === true) {
+                // continue
               }
-            ]
-          });
-
-          modalInstance.result.then(function(success) {
-            if (success === true) {
-              // continue
-            } 
-          }, function() {
-            // This gets called if modal gets dismissed
-            // when user clicks outside modal, etc.
-          });
-
-        };
-
-      }
-    ],
-    link: CommonFuncts.updateUser,
-    replace: true,
-    restrict: "E",
-    templateUrl: 'tribe_client/tribe-login-button.tpl.html'
-  };
-}])
+            }, function() {
+              // This gets called if modal gets dismissed
+              // when user clicks outside modal, etc.
+            });
+          };
+        }
+      ],
+      link: CommonFuncts.updateUser,
+      replace: true,
+      restrict: "E",
+      templateUrl: 'tribe_client/tribe-login-button.tpl.html'
+    };
+  }
+])
 
 // Directive for whole gene search form
 .directive('genesetSearchForm', function() {
   return {
+    controller: ['$scope', function($scope) {
+      // TODO RAZ: This is empty for now, but will be used for geneset search
+      // in the next feature & pull request.
+    }],
     replace: true,
     restrict: "E",
     templateUrl: 'tribe_client/geneset-search-form.tpl.html'
