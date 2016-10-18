86e07905b8c86427fff0529714b3cd02e790fce6
Rene Zelaya <Rene.Zelaya.F@gmail.com>
2016-10-18 13:21:08 -0400

    Making some changes in gene/search.js and gene/gene-search-form.tpl.html so that the user is warned that the Gene search is temporarily down if the query fails in the SearchResults.search() function.

--- interface/src/app/gene/search.js
+++ interface/src/app/gene/search.js
@@ -40,6 +40,7 @@ angular.module('adage.gene.search', [
 .factory('SearchResults', ['$rootScope', 'Gene', function($rootScope, Gene) {
   var queries = [];
   var searchResults = {};
+  var searchSuccess = null;
 
   return {
     getQueries: function() {
@@ -50,6 +51,9 @@ angular.module('adage.gene.search', [
     },
     getSearchResults: function() {
       return searchResults;
+    },
+    getSearchSuccess: function() {
+      return searchSuccess;
     },
     remove: function(query) { // Remove a query and its associated result
       queries = queries.filter(function(el) {
@@ -73,21 +77,28 @@ angular.module('adage.gene.search', [
       // contained terms already found in our cache of searchResults,
       // those results are ignored while terms not already present will
       // be added to the cache.
-      Gene.search(qparams, function(data) {
-        var previousQueries = queries.length;
-        for (var i = 0; i < data.length; i++) {
-          var query = data[i].search;
-          if (!searchResults[query]) {
-            // If search term didn't already exist
-            searchResults[query] = data[i]; // add it to the results
-            queries.push(query); // add to the list of queries
+      Gene.search(qparams,
+        function(data) {
+          var previousQueries = queries.length;
+          for (var i = 0; i < data.length; i++) {
+            var query = data[i].search;
+            if (!searchResults[query]) {
+              // If search term didn't already exist
+              searchResults[query] = data[i]; // add it to the results
+              queries.push(query); // add to the list of queries
+            }
           }
+          if (previousQueries !== queries.length) {
+            $rootScope.$broadcast('results.update');
+          }
+          searchSuccess = true;
+          $rootScope.$broadcast('results.searchResultsReturned');
+        },
+        function(responseObject, responseHeaders) {
+          searchSuccess = false;
+          $rootScope.$broadcast('results.searchResultsReturned');
         }
-        if (previousQueries !== queries.length) {
-          $rootScope.$broadcast('results.update');
-        }
-        $rootScope.$broadcast('results.searchResultsReturned');
-      });
+      );
     }
   };
 }])
@@ -152,6 +163,10 @@ angular.module('adage.gene.search', [
 
         $scope.$on('results.searchResultsReturned', function() {
           $scope.loadingSearchResults = false;
+          var searchSuccess = SearchResults.getSearchSuccess();
+          if (searchSuccess === null || searchSuccess === false) {
+            $scope.errors = 'Gene search is temporarily down';
+          }
         });
       }
     ],
