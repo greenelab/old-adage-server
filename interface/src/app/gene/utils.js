515bff81c49bfb2472b09fd13e2c504f753bc45c
Rene Zelaya <Rene.Zelaya.F@gmail.com>
2016-10-31 16:51:36 -0400

    Adding comment for getGeneLabel() function.

--- interface/src/app/gene/utils.js
+++ interface/src/app/gene/utils.js
@@ -17,6 +17,7 @@ angular.module('adage.gene.utils', [
     },
 
     getGeneLabel: function(gene) {
+      // Returns standard_name if it exists and systematic_name otherwise.
       var geneLabel = gene.systematic_name;
 
       if (gene.standard_name) {
