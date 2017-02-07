109ef73bbe16174cc26c63fc254253edd1b06606
Rene Zelaya <Rene.Zelaya.F@gmail.com>
2017-02-07 17:02:09 -0500

    Condensing 5 it() calls into one in utils.spec.js test.

--- interface/src/app/utils.spec.js
+++ interface/src/app/utils.spec.js
@@ -6,16 +6,16 @@
 describe('Test for hypergeometric test function in utils.js', function() {
   beforeEach(module('adage.utils'));
 
-  var mockService;
+  var MathFuncts;
 
   beforeEach(function() {
-    angular.mock.inject(function($injector) {
-      mockService = $injector.get('MathFuncts');
+    inject(function($injector) {
+      MathFuncts = $injector.get('MathFuncts');
     });
   });
 
   it('should be defined', function() {
-    expect(mockService).toBeDefined();
+    expect(MathFuncts).toBeDefined();
   });
 
   // The following functions will make sure that the output for the
@@ -23,28 +23,22 @@ describe('Test for hypergeometric test function in utils.js', function() {
   // http://stattrek.com/m/online-calculator/hypergeometric.aspx and
   // https://www.geneprof.org/GeneProf/tools/hypergeometric.jsp for
   // some given k values.
-  it('should output a desired p-value for k=1', function() {
-    expect(+(1 - mockService.hyperGeometricTest(1, 5, 10, 50)).toFixed(10))
-    .toEqual(0.689437218);
-  });
+  it('should output the desired p-values when k is an integer between 1 and 5',
+    function() {
+      expect(+(1 - MathFuncts.hyperGeometricTest(1, 5, 10, 50)).toFixed(10))
+      .toEqual(0.689437218);
 
-  it('should output a desired p-value for k=2', function() {
-    expect(+(1 - mockService.hyperGeometricTest(2, 5, 10, 50)).toFixed(10))
-    .toEqual(0.2581000208);
-  });
+      expect(+(1 - MathFuncts.hyperGeometricTest(2, 5, 10, 50)).toFixed(10))
+      .toEqual(0.2581000208);
 
-  it('should output a desired p-value for k=3', function() {
-    expect(+(1 - mockService.hyperGeometricTest(3, 5, 10, 50)).toFixed(10))
-    .toEqual(0.0482603032);
-  });
+      expect(+(1 - MathFuncts.hyperGeometricTest(3, 5, 10, 50)).toFixed(10))
+      .toEqual(0.0482603032);
 
-  it('should output a desired p-value for k=4', function() {
-    expect(+(1 - mockService.hyperGeometricTest(4, 5, 10, 50)).toFixed(10))
-    .toEqual(0.0040835205);
-  });
+      expect(+(1 - MathFuncts.hyperGeometricTest(4, 5, 10, 50)).toFixed(10))
+      .toEqual(0.0040835205);
 
-  it('should output a desired p-value for k=5', function() {
-    expect(+(1 - mockService.hyperGeometricTest(5, 5, 10, 50)).toFixed(10))
-    .toEqual(0.0001189375);
-  });
+      expect(+(1 - MathFuncts.hyperGeometricTest(5, 5, 10, 50)).toFixed(10))
+      .toEqual(0.0001189375);
+    }
+  );
 });
