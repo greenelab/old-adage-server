e8c947ca489ae532c011a176df7f82db9505c763
Rene Zelaya <Rene.Zelaya.F@gmail.com>
2017-02-08 11:27:35 -0500

    Adding some more documentation for functions that run the hypergeometric test in greenelab/stats.js

--- interface/src/app/greenelab/stats.js
+++ interface/src/app/greenelab/stats.js
@@ -11,6 +11,10 @@ angular.module('greenelab.stats', [
      * https://bitbucket.org/cgreene/integrator/src/1194f7ae1b11057a4649114e1f55570542acbcde/integrator/static/js/imp.js?at=giant&fileviewer=file-view-default
     */
     binomLog: function(n, k) {
+      // The 'n' and 'k' arguments are the same as the 'n' and 'k' arguments
+      // arguments for the hyperGeometricTest function below (the number of
+      // items in the sample and the number of successes in the sample,
+      // respectively).
       var coeff = 0;
       for (var i = n - k + 1; i <= n; i++) {
         coeff += Math.log(i);
@@ -26,6 +30,11 @@ angular.module('greenelab.stats', [
       // m is the number of successes in the population
       // n is the number of items in the sample
       // N is the number of items in the population
+      //
+      // The names of these variables are chosen based partly on the variable
+      // names in the /integrator/static/js/imp.js script mentioned above,
+      // but are also common in online descriptions of the hypergeometric test.
+      // See: https://en.wikipedia.org/wiki/Hypergeometric_distribution#Application_and_example
       var sum = 0;
       for (var i = 0; i < k; i++) {
         var logSum = this.binomLog(m, i) +
