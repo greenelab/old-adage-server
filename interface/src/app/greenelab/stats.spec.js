32b50c7540af7f0a02f18dcbebd5aff3a451b6dd
Matt Huyck <mhuyck@fgtech.com>
2017-02-15 13:00:48 -0500

    Use .toBeCloseTo() instead of .toFixed() and .toEqual()
    Another good idea from Dongbo. This way is simpler!

--- interface/src/app/greenelab/stats.spec.js
+++ interface/src/app/greenelab/stats.spec.js
@@ -23,20 +23,20 @@ describe('Test the hypergeometric test function in stats.js', function() {
   // some given k values.
   it('should output the desired p-values when k is an integer between 1 and 5',
     function() {
-      expect(+(1 - MathFuncts.hyperGeometricTest(1, 5, 10, 50)).toFixed(10))
-      .toEqual(0.689437218);
+      expect(+(1 - MathFuncts.hyperGeometricTest(1, 5, 10, 50)))
+      .toBeCloseTo(0.689437218, 10);
 
-      expect(+(1 - MathFuncts.hyperGeometricTest(2, 5, 10, 50)).toFixed(10))
-      .toEqual(0.2581000208);
+      expect(+(1 - MathFuncts.hyperGeometricTest(2, 5, 10, 50)))
+      .toBeCloseTo(0.2581000208, 10);
 
-      expect(+(1 - MathFuncts.hyperGeometricTest(3, 5, 10, 50)).toFixed(10))
-      .toEqual(0.0482603032);
+      expect(+(1 - MathFuncts.hyperGeometricTest(3, 5, 10, 50)))
+      .toBeCloseTo(0.0482603032, 10);
 
-      expect(+(1 - MathFuncts.hyperGeometricTest(4, 5, 10, 50)).toFixed(10))
-      .toEqual(0.0040835205);
+      expect(+(1 - MathFuncts.hyperGeometricTest(4, 5, 10, 50)))
+      .toBeCloseTo(0.0040835205, 10);
 
-      expect(+(1 - MathFuncts.hyperGeometricTest(5, 5, 10, 50)).toFixed(10))
-      .toEqual(0.0001189375);
+      expect(+(1 - MathFuncts.hyperGeometricTest(5, 5, 10, 50)))
+      .toBeCloseTo(0.0001189375, 10);
     }
   );
 });
@@ -68,12 +68,12 @@ describe('tTest in stats.js', function() {
       [0.7, -1.6, -0.2, -1.2, -0.1, 3.4, 3.7, 0.8, 0.0, 2.0],
       [1.9, 0.8, 1.1, 0.1, -0.1, 4.4, 5.5, 1.6, 4.6, 3.4]
     );
-    expect(+tt.testValue().toFixed(10)).toEqual(-1.8608134675);
-    expect(+tt.pValue().toFixed(10)).toEqual(0.0793941404);
-    expect(+tt.confidence()[0].toFixed(10)).toEqual(-3.3654832104); // lower ci
-    expect(+tt.confidence()[1].toFixed(10)).toEqual(0.2054832104); // upper ci
+    expect(+tt.testValue()).toBeCloseTo(-1.8608134675, 10);
+    expect(+tt.pValue()).toBeCloseTo(0.0793941404, 10);
+    expect(+tt.confidence()[0]).toBeCloseTo(-3.3654832104, 10); // lower ci
+    expect(+tt.confidence()[1]).toBeCloseTo(0.2054832104, 10); // upper ci
     expect(tt.valid()).toBe(true);
-    expect(+tt.freedom().toFixed(10)).toEqual(17.7764735162);
+    expect(+tt.freedom()).toBeCloseTo(17.7764735162, 10);
 
     // Compare with R's output from:
     // tt <- t.test(c(0.5361161, 0.3878819, -0.2688780, -0.3422277, 2.4233964),
@@ -82,12 +82,12 @@ describe('tTest in stats.js', function() {
       [0.5361161, 0.3878819, -0.2688780, -0.3422277, 2.4233964],
       [-1.9037639, -2.1121372, 1.9007528, 0.2987301, -1.0119501]
     );
-    expect(+tt.testValue().toFixed(10)).toEqual(1.2360437329);
-    expect(+tt.pValue().toFixed(10)).toEqual(0.2564335358);
-    expect(+tt.confidence()[0].toFixed(10)).toEqual(-1.0175996584); // lower ci
-    expect(+tt.confidence()[1].toFixed(10)).toEqual(3.2434624584); // upper ci
+    expect(+tt.testValue()).toBeCloseTo(1.2360437329, 10);
+    expect(+tt.pValue()).toBeCloseTo(0.2564335358, 10);
+    expect(+tt.confidence()[0]).toBeCloseTo(-1.0175996584, 10); // lower ci
+    expect(+tt.confidence()[1]).toBeCloseTo(3.2434624584, 10); // upper ci
     expect(tt.valid()).toBe(true);
-    expect(+tt.freedom().toFixed(10)).toEqual(6.9769307501);
+    expect(+tt.freedom()).toBeCloseTo(6.9769307501, 10);
 
     // Compare with R's output from:
     // tt <- t.test(c(0.002297313, -2.01655, 0.8762991, -0.04565538, 0.4066173,
@@ -112,11 +112,11 @@ describe('tTest in stats.js', function() {
         -0.1429274, -1.328964, 1.668616, -0.2354719, -0.3779898, 1.169342,
         1.557759, 1.793148, -1.409778, 1.660374, 2.101568, 1.057995]
     );
-    expect(+tt.testValue().toFixed(10)).toEqual(-3.3209761128);
-    expect(+tt.pValue().toFixed(10)).toEqual(0.0015663897);
-    expect(+tt.confidence()[0].toFixed(10)).toEqual(-1.3411325516); // lower ci
-    expect(+tt.confidence()[1].toFixed(10)).toEqual(-0.3322217752); // upper ci
+    expect(+tt.testValue()).toBeCloseTo(-3.3209761128, 10);
+    expect(+tt.pValue()).toBeCloseTo(0.0015663897, 10);
+    expect(+tt.confidence()[0]).toBeCloseTo(-1.3411325516, 10); // lower ci
+    expect(+tt.confidence()[1]).toBeCloseTo(-0.3322217752, 10); // upper ci
     expect(tt.valid()).toBe(false);
-    expect(+tt.freedom().toFixed(10)).toEqual(57.209217643);
+    expect(+tt.freedom()).toBeCloseTo(57.209217643, 10);
   });
 });
