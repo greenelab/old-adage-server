16ca9a89f5f6c907a37808d24362f9438701cfa5
Dongbo <dongbo.hu@gmail.com>
2017-02-16 16:05:50 -0500

    Move  calls into separate services, create self.precision to control the # of digits in activity values

--- interface/src/app/sample_annotation/annotation.js
+++ interface/src/app/sample_annotation/annotation.js
@@ -5,7 +5,7 @@
 angular.module('adage.sampleAnnotation', [
   'ui.router',
   'ui.bootstrap',
-  'greenelab.utils'
+  'adage.utils'
 ])
 
 .config(['$stateProvider', function($stateProvider) {
@@ -21,9 +21,21 @@ angular.module('adage.sampleAnnotation', [
   });
 }])
 
+.factory('ActivityService', ['$resource', 'ApiBasePath',
+  function($resource, ApiBasePath) {
+    return $resource(ApiBasePath + 'activity');
+  }
+])
+
+.factory('SampleService', ['$resource', 'ApiBasePath',
+  function($resource, ApiBasePath) {
+    return $resource(ApiBasePath + 'sample');
+  }
+])
+
 .controller('SampleAnnotationCtrl', [
-  '$stateParams', '$q', '$http', '$log', 'ApiBasePath', 'errGen',
-  function($stateParams, $q, $http, $log, ApiBasePath, errGen) {
+  '$stateParams', '$q', '$log', 'errGen', 'ActivityService', 'SampleService',
+  function($stateParams, $q, $log, errGen, ActivityService, SampleService) {
     var self = this;
     self.queryStatus = 'Connecting to the server ...';
 
@@ -44,26 +56,27 @@ angular.module('adage.sampleAnnotation', [
     // Function that sets valid sample IDs asynchronously:
     var getSampleIDPromise = function() {
       var deferred = $q.defer();
-      // If 'node' is specified in the URL, query the node first:
+      // If 'node' is found in the URL, get the activity values based on node
+      // and sample(s):
       if ($stateParams.node) {
         self.hasNode = true;
+        self.precision = 5;
         var nodeID = $stateParams.node;
-        var activityApiConfig = {
-          params: {'node': nodeID, 'sample__in': samplesInUrl}
-        };
-        $http.get(ApiBasePath + 'activity/', activityApiConfig)
-          .then(function success(response) {
+        ActivityService.get(
+          {'node': nodeID, 'sample__in': samplesInUrl},  // parameters of GET
+          function success(response) {
             var validSamples = [];
-            response.data.objects.forEach(function(element) {
+            response.objects.forEach(function(element) {
               validSamples.push(element.sample);
               sampleDict[element.sample] = {activity: element.value};
             });
             deferred.resolve(validSamples.join());
-          }, function error(response) {
+          },
+          function error(response) {
             var errMessage = errGen('Failed to get activities', response);
             $log.error(errMessage);
             self.queryStatus = errMessage + '. Please try again later.';
-            deferred.reject();
+            deferred.reject(errMessage);
           });
       } else { // If 'node' is not in the URL, include all input sample IDs.
         deferred.resolve(samplesInUrl);
@@ -72,46 +85,45 @@ angular.module('adage.sampleAnnotation', [
     };
 
     var sidPromise = getSampleIDPromise();
-    // The first then() block only includes a success handler function, because
-    // the error has been notified to the frontend by setting self.queryStatus.
+    // The following then() block only includes a success handler function,
+    // because the error has been reported by setting self.queryStatus.
     sidPromise.then(function success(validSamples) {
-      var sampleApiConfig = {
-        params: {'id__in': validSamples, 'limit': 0}
-      };
-      return $http.get(ApiBasePath + 'sample/', sampleApiConfig);
-    }).then(function success(response) {
-      // If no valid sample records are found, report the error.
-      if (!response.data.objects.length) {
-        self.queryStatus = 'Valid samples not found';
-        return;
-      }
-      response.data.objects.forEach(function(element) {
-        if (!self.hasNode) { // If node is not in URL, create the object first
-          sampleDict[element.id] = {};
-        }
-        sampleDict[element.id].name = element.name;
-        sampleDict[element.id].annotations = element.annotations;
-        var currentTypes = Object.keys(element.annotations);
-        currentTypes.forEach(function(annotationType) {
-          if (self.uniqueAnnotationTypes.indexOf(annotationType) === -1) {
-            self.uniqueAnnotationTypes.push(annotationType);
+      SampleService.get(
+        {'id__in': validSamples, 'limit': 0}, // parameters of GET
+        function success(response) {
+          // If no valid sample records are found, report the error.
+          if (!response.objects.length) {
+            self.queryStatus = 'Valid samples not found';
+            return;
           }
-        });
-      });
+          response.objects.forEach(function(element) {
+            if (!self.hasNode) { // If node is not in URL, create the object.
+              sampleDict[element.id] = {};
+            }
+            sampleDict[element.id].name = element.name;
+            sampleDict[element.id].annotations = element.annotations;
+            var currentTypes = Object.keys(element.annotations);
+            currentTypes.forEach(function(annotationType) {
+              if (self.uniqueAnnotationTypes.indexOf(annotationType) === -1) {
+                self.uniqueAnnotationTypes.push(annotationType);
+              }
+            });
+          });
 
-      // Convert sampleDict from an object literal to an array and save it as
-      // self.samples. (It has to be an array so that samples can be shown on
-      // web UI using "orderBy" filter in Angular.
-      self.samples = [];
-      Object.keys(sampleDict).map(function(key) {
-        self.samples.push(sampleDict[key]);
-      });
-      self.uniqueAnnotationTypes.sort(); // Sort the annotation types
-      self.queryStatus = '';
-    }, function error(response) {
-      var errMessage = errGen('Failed to get sample annotations', response);
-      $log.error(errMessage);
-      self.queryStatus = errMessage + '. Please try again later.';
-    }); // end of sidPromise.then().then() chaining
+          // Convert sampleDict from an object literal to an array and save
+          // it as self.samples. (It has to be an array so that the order of
+          // samples can be customized on web UI with "orderBy" filter.
+          self.samples = [];
+          Object.keys(sampleDict).map(function(key) {
+            self.samples.push(sampleDict[key]);
+          });
+          self.uniqueAnnotationTypes.sort(); // Sort the annotation types
+          self.queryStatus = '';
+        }, function error(response) {
+          var errMessage = errGen('Failed to get sample annotations', response);
+          $log.error(errMessage);
+          self.queryStatus = errMessage + '. Please try again later.';
+        });
+    }); // end of sidPromise.then()
   }
 ]);
