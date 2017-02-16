93b51a7942c9ed8e9859e8ee9ac9a8f018323348
Rene Zelaya <Rene.Zelaya.F@gmail.com>
2017-02-16 11:11:42 -0500

    Adding a 'limit' of 0 to the MLModel resource GET request in the mlModelSelector component, and also adding some documentation for it.

--- interface/src/app/mlmodel/mLModelComponents.js
+++ interface/src/app/mlmodel/mLModelComponents.js
@@ -10,7 +10,14 @@ angular.module('adage.mlmodel.components', [
   controller: ['MLModel', '$log', function(MLModel, $log) {
     var self = this;
 
-    MLModel.get(
+    // apiReturnLimit is what the 'limit' parameter will be set to in the
+    // MLModel resource's GET request. By setting it to 0, we are actually
+    // telling the Tastypie API that we want it to return ALL objects from
+    // this endpoint (i.e. that we do not want a maximum limit to how many
+    // objects it returns).
+    var apiReturnLimit = 0;
+
+    MLModel.get({limit: apiReturnLimit},
       function success(response) {
         self.availableMLModels = response.objects;
       },
