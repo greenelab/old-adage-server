a5eb6e8ff94a08a867ce7838b492a5e81102b8c1
Rene Zelaya <Rene.Zelaya.F@gmail.com>
2017-02-27 15:24:51 -0500

    Converting directives in tribe_client/genesets.js to Angular 'components', and fetching Gene objects from the database for every geneset retrieved from Tribe.

--- interface/src/app/tribe_client/genesets.js
+++ interface/src/app/tribe_client/genesets.js
@@ -1,77 +1,98 @@
 angular.module('adage.tribe_client.genesets', [
-  'adage.tribe_client.resource'
+  'adage.tribe_client.resource',
+  'adage.gene.resource'
 ])
 
-.directive('genesetSearchForm', [function() {
-  return {
-    controller: ['$scope', 'CommonGeneFuncts',
-      function($scope, CommonGeneFuncts) {
-        // Number of results (in this case genesets) to come back
-        // in each response page.
-        $scope.limit = 10;
-        $scope.sendToNetwork = function() {
-          CommonGeneFuncts.sendToNetwork($scope, $state);
-        };
-      }
-    ],
-    replace: true,
-    restrict: 'E',
-    scope: {
-      organism: '@'
-    },
-    templateUrl: 'tribe_client/geneset-search-form.tpl.html'
-  };
-}])
+// Wrapping component that houses the genesetSearchBar and the
+// genesetResultTable.
+.component('genesetSearchForm', {
+  templateUrl: 'tribe_client/geneset-search-form.tpl.html',
+  bindings: {
+    organism: '@',
+    mlModel: '@'
+  },
+  controller: ['$state', 'CommonGeneFuncts',
+    function($state, CommonGeneFuncts) {
+      var self = this;
+
+      // Number of results (in this case genesets) to come back
+      // in each response page.
+      self.limit = 10;
+      self.sendToNetwork = function() {
+        self.selectedGenes = {};
+
+        var genes = self.selectedGeneset.geneObjs;
+        for (var i = 0; i < genes.length; i++) {
+          self.selectedGenes[genes[i].id] = genes[i];
+        }
+        CommonGeneFuncts.sendToNetwork(self, $state);
+      };
+    }
+  ]
+})
 
 
 // Search box for users to enter their search text into
 // and go search Tribe genesets.
-.directive('genesetSearchBar', ['Genesets', function(Genesets) {
-  return {
-    controller: ['$scope', function($scope) {
-      $scope.search = {};
+.component('genesetSearchBar', {
+  templateUrl: 'tribe_client/geneset-search-bar.tpl.html',
+  bindings: {
+    limit: '@',
+    organism: '@',
+    genesets: '='
+  },
+  controller: ['$log', 'Genesets', 'Gene', function($log, Genesets, Gene) {
+    var self = this;
 
-      $scope.searchGenesets = function() {
-        var qparams = {};
-        qparams['query'] = $scope.search.query;
-        qparams['organism__scientific_name'] = $scope.organism;
-        qparams['limit'] = $scope.limit;
+    self.search = {};
 
-        Genesets.query(qparams, function(data) {
-          $scope.genesets = data.objects;
+    self.searchGenesets = function() {
+      var qparams = {};
+      qparams['query'] = self.search.query;
+      qparams['organism__scientific_name'] = self.organism;
+      qparams['limit'] = self.limit;
+
+      Genesets.query(qparams, function(data) {
+        self.genesets = [];
+        var returnedGenesets = data.objects;
+        angular.forEach(returnedGenesets, function(geneset) {
+          var entrezIds = geneset.tip.genes.join(',');
+          Gene.get({'entrezid__in': entrezIds},
+            function success(response) {
+              geneset.geneObjs = response.objects;
+              self.genesets.push(geneset);
+            },
+            function error(err) {
+              $log.error('Failed to get gene information: ' + err);
+              self.errorMessage = 'Failed to get gene information from server';
+            }
+          );
         });
-      };
-    }],
-    replace: true,
-    restrict: 'E',
-    scope: {
-      limit: '@',
-      organism: '@',
-      genesets: '='
-    },
-    templateUrl: 'tribe_client/geneset-search-bar.tpl.html'
-  };
-}])
+      });
+    };
+  }]
+})
 
 // Directive for table containing search results
-.directive('genesetResultTable', [function() {
-  return {
-    restrict: 'E',
-    scope: {
-      limit: '@',
-      organism: '@',
-      genesets: '=',
-      selectedGeneset: '='
-    },
-    templateUrl: 'tribe_client/geneset-result-table.tpl.html',
-    link: function($scope) {
-      $scope.selectedGeneset = {};
-      // TODO: Implement pagination for geneset search results in
-      // case the number of results ($scope.genesetResultCount above)
-      // is greater than this 'limit' parameter (the maximum number of
-      // geneset results that the Tribe API should return at once).
-    }
-  };
-}])
+.component('genesetResultTable', {
+  templateUrl: 'tribe_client/geneset-result-table.tpl.html',
+  bindings: {
+    limit: '@',
+    organism: '@',
+    genesets: '=',
+    selectedGeneset: '='
+  },
+  controller: ['CommonGeneFuncts', function(CommonGeneFuncts) {
+    var self = this;
+    self.selectedGeneset = {};
+
+    self.getGeneLabelList = function(geneList) {
+      var geneLabelList = geneList.map(function(gene) {
+        return CommonGeneFuncts.getGeneLabel(gene);
+      }).join(' ');
+      return geneLabelList;
+    };
+  }]
+})
 
 ;
