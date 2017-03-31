d88a33e2d21db74507432198da550a6286018ee8
Matt Huyck <mhuyck@fgtech.com>
2017-03-30 21:42:29 -0400

    Convert .directive()s to .component()s.

--- interface/src/app/volcano-plot/volcano-plot.js
+++ interface/src/app/volcano-plot/volcano-plot.js
@@ -9,75 +9,73 @@ angular.module('adage.volcano-plot', [
   'adage.volcano-plot-vgspec'
 ])
 
-.controller('VolcanoPlotCtrl', ['$scope', 'VolcanoPlotSpec',
-  function VolcanoPlotCtrl($scope, VolcanoPlotSpec) {
-    // define controller instance variables that link with vega
-    this.spec = VolcanoPlotSpec;
-    this.data = $scope.plotData;
-    // FIXME: can't get selectedNodes out this way... need to use view api?
-    // $scope.plotData.selectedNodes: $scope.selectedNodes
-    this.viewListener = function(view) {
-      console.log('viewListener fired');
-      view.on('click', function(event, item) {
-        var selectedNodes = view.data('selectedNodes').values();
-        // console.log(JSON.stringify(selectedNodes));
-        selectedNodes = selectedNodes.reduce(
-          function(acc, datum) {
-            if (datum['id']) {
-              var datumCopy = angular.copy(datum);
-              delete datumCopy['_id'];
-              delete datumCopy['_prev'];
-              acc.push(datumCopy);
-            }
-            return acc;
-          },
-          []
-        );
-        console.log(JSON.stringify(selectedNodes));
-      });
-    };
-  }
-])
-
-.directive('volcanoPlot', function() {
-  return {
-    restrict: 'E',
-    scope: {
-      plotData: '=',
-        // plotData is a json list with the following format:
-        // [
-        //   {
-        //     'node': "<node-name-1>",
-        //     'diff': "<difference-in-mean-activity>",
-        //     'logsig': "<-log10(p-value-of-difference)>",
-        //   },
-        //   {...repeat for each node...}
-        // ]
-      selectedNodes: '='
-        // selectedNodes is a json list with the following format:
-        // [
-        //   {
-        //     'node': "<node-name-1>"
-        //   },
-        //   {
-        //     'node': "<node-name-2>"
-        //   },
-        //   {...repeat for each node...}
-        // ]
-    },
-    templateUrl: 'volcano-plot/volcano-plot.tpl.html',
-    controller: 'VolcanoPlotCtrl as vp'
-  };
+.component('volcanoPlot', {
+  bindings: {
+    plotData: '<',
+      // plotData is a json list with the following format:
+      // [
+      //   {
+      //     'node': "<node-name-1>",
+      //     'diff': "<difference-in-mean-activity>",
+      //     'logsig': "<-log10(p-value-of-difference)>",
+      //   },
+      //   {...repeat for each node...}
+      // ]
+    selectedNodes: '='
+      // selectedNodes is a json list with the following format:
+      // [
+      //   {
+      //     'node': "<node-name-1>"
+      //   },
+      //   {
+      //     'node': "<node-name-2>"
+      //   },
+      //   {...repeat for each node...}
+      // ]
+  },
+  templateUrl: 'volcano-plot/volcano-plot.tpl.html',
+  controllerAs: 'vp',
+  controller: ['VolcanoPlotSpec',
+    function VolcanoPlotCtrl(VolcanoPlotSpec) {
+      // define controller instance variables that link with vega
+      this.spec = VolcanoPlotSpec;
+      this.data = this.plotData;
+      // FIXME: can't get selectedNodes out this way... need to use view api?
+      // $scope.plotData.selectedNodes: $scope.selectedNodes
+      this.viewListener = function(view) {
+        console.log('viewListener fired');
+        view.on('click', function(event, item) {
+          selectedNodes = view.data('selectedNodes').values();
+          // console.log(JSON.stringify(selectedNodes));
+          selectedNodes = selectedNodes.reduce(
+            function(acc, datum) {
+              if (datum['id']) {
+                var datumCopy = angular.copy(datum);
+                delete datumCopy['_id'];
+                delete datumCopy['_prev'];
+                acc.push(datumCopy);
+              }
+              return acc;
+            },
+            []
+          );
+          console.log(JSON.stringify(selectedNodes));
+        });
+      };
+    }
+  ]
 })
 
-.directive('volcanoPlotSelection', function() {
-  return {
-    restrict: 'E',
-    scope: {
-      selectedNodes: '='
-    },
-    templateUrl: 'volcano-plot/volcano-plot-selection.tpl.html'
-  };
+.component('volcanoPlotSelection', {
+  bindings: {
+    selectedNodes: '<'
+  },
+  templateUrl: 'volcano-plot/volcano-plot-selection.tpl.html',
+  controller: function volcanoPlotSelectionCtrl() {
+    this.$onInit = function() {
+      console.log('volcanoPlotSelectionCtrl init');
+    };
+  }
 })
 
 ;
