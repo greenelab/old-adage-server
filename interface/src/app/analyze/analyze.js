2ce8d3a65f683c450281230769ff1917af2cb389
Matt Huyck <mhuyck@fgtech.com>
2017-07-21 16:23:02 -0400

    [FEAT] add tooltips to search_item "add" button

--- interface/src/app/analyze/analyze.js
+++ interface/src/app/analyze/analyze.js
@@ -28,8 +28,9 @@ angular.module('adage.analyze', [
 })
 
 .controller('AnalyzeCtrl', ['$scope', '$stateParams', '$log', '$location',
-  'Sample',
-  function AnalyzeCtrl($scope, $stateParams, $log, $location, Sample) {
+  'Sample', 'SampleBin',
+  function AnalyzeCtrl($scope, $stateParams, $log, $location, Sample,
+                       SampleBin) {
     $scope.isValidModel = false;
     // Do nothing if mlmodel in URL is falsey. The error will be taken
     // care of by "<ml-model-validator>" component.
@@ -50,6 +51,21 @@ angular.module('adage.analyze', [
           classList += ' active';
         }
         return classList;
+      },
+
+      item_tooltip: function(search_item) {
+        // Determine what text to show as a tooltip for this search_item.
+        if (SampleBin.hasItem(search_item)) {
+          return 'already added to analysis';
+        }
+        if (search_item.item_type === 'sample') {
+          return 'add this sample to analysis';
+        }
+        if (search_item.item_type === 'experiment') {
+          return 'add these samples to analysis';
+        }
+        $log.warn('item_tooltip: unknown search item_type', search_item);
+        return 'unknown search item type';
       },
 
       scroll_to_id: function(id) {
