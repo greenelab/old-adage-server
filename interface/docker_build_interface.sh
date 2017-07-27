31303710976b61b9df83313b95813a272dbd1a9d
Rene Zelaya <Rene.Zelaya.F@gmail.com>
2017-07-27 17:07:48 -0400

    Adding shell script to build the interface using Dockerfile

---
+++ interface/docker_build_interface.sh
@@ -0,0 +1,21 @@
+docker build -t adage-server/docker-interface-base -f Dockerfile.base .
+
+docker build -t adage-server/docker-interface-build -f Dockerfile .
+
+# Set up mounting volume directory for bin and build folder if it does
+# not already exist
+script_directory=`dirname "${BASH_SOURCE[0]}"  | xargs realpath`
+compile_folder="$script_directory/compile_folder"
+
+if [ ! -d "$compile_folder" ]; then
+    mkdir $compile_folder
+    chmod 775 $compile_folder
+fi
+
+docker run \
+      --volume $compile_folder:/home/user/compile_folder \
+      adage-server/docker-interface-build
+
+mv $compile_folder/bin ./
+
+rmdir $compile_folder
