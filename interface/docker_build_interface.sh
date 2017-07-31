4013f3af39807f32d94e142c7293feb623954d85
Rene Zelaya <Rene.Zelaya.F@gmail.com>
2017-07-31 11:13:40 -0400

    Adding some comments to docker_build_interface.sh file to make the reason for having a parent 'compile_folder' clearer.

--- interface/docker_build_interface.sh
+++ interface/docker_build_interface.sh
@@ -2,8 +2,8 @@ docker build -t adage-server/docker-interface-base -f Dockerfile.base .
 
 docker build -t adage-server/docker-interface-build -f Dockerfile .
 
-# Set up mounting volume directory for bin and build folder if it does
-# not already exist
+# Set up mounting volume directory that will house bin and build folders
+# if it doesn't already exist.
 script_directory=`dirname "${BASH_SOURCE[0]}"  | xargs realpath`
 compile_folder="$script_directory/compile_folder"
 
@@ -12,6 +12,11 @@ if [ ! -d "$compile_folder" ]; then
     chmod 775 $compile_folder
 fi
 
+# The reason we mount the parent "compile_folder" and then move the
+# "bin" and "build" folders to the current directory is that Docker will
+# not allow the mounting points or folders themselves to be deleted.
+# However, deleting these folders is exactly what the "grunt" command needs
+# to do right before it builds and compiles the interface files.
 docker run \
       --volume $compile_folder:/home/user/compile_folder \
       adage-server/docker-interface-build
