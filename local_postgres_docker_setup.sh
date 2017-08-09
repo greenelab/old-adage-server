94d52b43bd04e1869d803982a0619aa576104549
Rene Zelaya <Rene.Zelaya.F@gmail.com>
2017-08-09 14:34:30 -0400

    Breaking up long lines and correcting some comments in local_postgres_docker_setup.sh.

--- local_postgres_docker_setup.sh
+++ local_postgres_docker_setup.sh
@@ -2,11 +2,11 @@
 
 # Copied from https://github.com/data-refinery/data_refinery/blob/master/data_models/install.sh
 
-# Run this script with 'sudo -u local_postgres_docker_setup.sh'
+# Run this script with 'sudo ./local_postgres_docker_setup.sh'
 
 # This script defaults to postgres version 9.3.
 # To use a different version set the POSTGRES_VERSION env var
-# (i.e. 'sudo POSTGRES_VERSION=9.4 -u local_postgres_docker_setup.sh')
+# (i.e. 'sudo POSTGRES_VERSION=9.4 ./local_postgres_docker_setup.sh')
 if [ -z $POSTGRES_VERSION ]; then POSTGRES_VERSION=9.3; fi
 
 # See http://unix.stackexchange.com/questions/52376/why-do-iptables-rules-disappear-when-restarting-my-debian-system
@@ -23,9 +23,11 @@ iptables -A INPUT -s 172.17.0.0/16 -j ACCEPT
 # and https://blog.jsinh.in/how-to-enable-remote-access-to-postgresql-database-server/#.WNFiBCErLmG
 # for more information on these two settings.
 # 172.17.0.0/16 is the range of addresses that docker gives its containers
-echo 'host    all             all             172.17.0.0/16           md5' >> /etc/postgresql/$POSTGRES_VERSION/main/pg_hba.conf
+echo 'host    all             all             172.17.0.0/16           md5' >> \
+    /etc/postgresql/$POSTGRES_VERSION/main/pg_hba.conf
 
-sed -i "s/#\(listen_addresses\)\( = 'localhost'\)/\1 = '*'/" /etc/postgresql/$POSTGRES_VERSION/main/postgresql.conf
+sed -i "s/#\(listen_addresses\)\( = 'localhost'\)/\1 = '*'/" \
+    /etc/postgresql/$POSTGRES_VERSION/main/postgresql.conf
 
 # If this is added to a running system, do
 sudo service postgresql restart
