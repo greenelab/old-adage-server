324b17fbc984699e749414bd317bc56001eda3a4
Rene Zelaya <Rene.Zelaya.F@gmail.com>
2017-09-12 12:28:39 -0400

    Include command to build the interface in docker_local_deploy.sh

--- docker_local_deploy.sh
+++ docker_local_deploy.sh
@@ -3,4 +3,7 @@
 # *Note - This way of deploying assumes both Docker and docker-compose
 # have been installed in the current environment
 
+# Build the interface
+interface/docker_build_interface.sh
+
 docker-compose up
