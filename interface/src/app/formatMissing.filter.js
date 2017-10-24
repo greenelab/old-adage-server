d123865b30453c2e74ec1685e15e72a3003a3c5b
Matt Huyck <mhuyck@fgtech.com>
2017-10-24 15:53:21 -0400

    [FIX] clarify a couple of points from code review feedback
    Thank you Dongbo!

--- interface/src/app/formatMissing.filter.js
+++ interface/src/app/formatMissing.filter.js
@@ -1,9 +1,12 @@
 angular.module('adage.formatMissing.filter', [])
 
 .filter('formatMissing', function() {
-  return function(inputStr, replacementStr) {
+  return function(inputStr, substituteStr) {
     // default to 'N/A'
-    var replacement = (replacementStr === undefined ? 'N/A' : replacementStr);
+    var replacement = (substituteStr === undefined ? 'N/A' : substituteStr);
+    // The crazy RegEx below performs a "String.trim", but should work in more
+    // browsers. Credit due to Mozilla for the documentation at:
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/Trim#Polyfill
     if (inputStr.replace(/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g, '') === '') {
       return replacement;
     } else {
